ce31cf97b4a77bd0250151cebe69593b8728281f
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-06-12 15:53:15 +0530

    interface class and inheritance

--- interfaces/app.js
+++ interfaces/app.js
@@ -16,4 +16,31 @@ greet(person);
 changeName(person);
 greet(person);
 person.greet("Anything");
+var Person = /** @class */ (function () {
+    function Person() {
+    }
+    Person.prototype.greet = function (lastName) {
+        console.log("Hi, I am " + this.firstName + " " + lastName);
+    };
+    ;
+    return Person;
+}());
+var myPerson = new Person();
+myPerson.firstName = "Nirmit";
+myPerson.lastName = "Maheshwari";
+greet(myPerson);
+myPerson.greet("Anything");
+var myDoubleFunction;
+myDoubleFunction = function (value1, value2) {
+    return (value1 + value2) * 2;
+};
+console.log(myDoubleFunction(10, 20));
+var oldPerson = {
+    age: 27,
+    firstName: "Max",
+    greet: function (lastName) {
+        console.log("Hello!");
+    }
+};
+console.log(oldPerson);
 //# sourceMappingURL=app.js.map
--- interfaces/app.ts
+++ interfaces/app.ts
@@ -26,3 +26,46 @@ greet(person);
 changeName(person);
 greet(person);
 person.greet("Anything");
+
+class Person implements NamePerson {
+	firstName: string;
+	lastName: string;
+	greet(lastName: string) {
+		console.log("Hi, I am " + this.firstName+ " " + lastName);
+	};
+}
+
+const myPerson = new Person();
+myPerson.firstName = "Nirmit";
+myPerson.lastName = "Maheshwari";
+greet(myPerson);
+myPerson.greet("Anything");
+
+// Function Types
+
+interface DoubleValueFunc {
+	(number1:number, number2:number):number;
+}
+
+let myDoubleFunction : DoubleValueFunc;
+myDoubleFunction = function (value1:number, value2:number) {
+	return (value1+value2)*2;
+};
+
+console.log(myDoubleFunction(10,20));
+
+// Interface Inheritance
+
+interface AgedPerson extends NamePerson {
+	age:number;
+}
+
+const oldPerson : AgedPerson = {
+	age:27,
+	firstName : "Max",
+	greet(lastName: string) {
+		console.log("Hello!");
+	}
+};
+
+console.log(oldPerson);
